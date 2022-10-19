ef142a736e46abf264696ab5c22e51c96507b538
Ilham Mohomed <85997394+d12ilham@users.noreply.github.com>
2022-10-19 07:49:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".__main-section-top-categories-wrapper-mobile", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  loop: true,
+
+  pagination: {
+    el: ".categories-swiper-pagination",
+    clickable: true,
+  },
+});
